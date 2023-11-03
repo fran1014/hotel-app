7b6cc62e44f227f0c601efa363f8bd3189566ada
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-03 22:16:57 +0100

    added all crud operations to the service

--- src/app/reservation/reservation.service.ts
+++ src/app/reservation/reservation.service.ts
@@ -8,6 +8,8 @@ export class ReservationService {
 
   private reservations: Reservation[] = [];
 
+  //CRUD Operations
+
   getReservations(): Reservation[]{
     return this.reservations;
   }
@@ -16,5 +18,20 @@ export class ReservationService {
     return this.reservations.find(res=> res.id === id);
   }
 
+  addReservation (reservation : Reservation) : void {
+    this.reservations.push(reservation);
+  }
+
+  deleteReservation (id:string) : void {
+    let index = this.reservations.findIndex(res => res.id === id);
+    this.reservations.splice(index,1)
+  }
+
+  updateReservation(updatedReservation:Reservation): void{
+    let index = this.reservations.findIndex(res=> res.id === updatedReservation.id);
+    this.reservations [index] = updatedReservation;
+
+  }
   
 }
+
