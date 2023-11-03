278ef7f48d683fd37e8d3adf416767b03188f631
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-03 20:27:40 +0100

    created reservation list and form component

--- src/app/reservation/reservation.module.ts
+++ src/app/reservation/reservation.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReservationFormComponent } from '../reservation-form/reservation-form.component';
+import { ReservationListComponent } from '../reservation-list/reservation-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ReservationFormComponent,
+    ReservationListComponent
+  ],
   imports: [
     CommonModule
   ]
