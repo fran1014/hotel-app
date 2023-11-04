d7a5ad3657027bb8f9f403646571de3a28956b68
Franklin <71532004+fran1014@users.noreply.github.com>
2023-11-04 17:44:24 +0100

    patching value of reservation id

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { ReservationService } from '../reservation/reservation.service';
 import { Reservation } from '../models/reservation';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-reservation-form',
@@ -16,7 +16,8 @@ export class ReservationFormComponent implements OnInit{
   constructor(
     private formBuilder: FormBuilder,
     private reservationService : ReservationService,
-    private router:Router){}
+    private router:Router,
+    private activatedRoute:ActivatedRoute){}
 
   ngOnInit(): void {
     this.reservationForm = this.formBuilder.group({
@@ -26,6 +27,15 @@ export class ReservationFormComponent implements OnInit{
       guestEmail: ['', [Validators.required, Validators.email]],
       roomNumber: ['', Validators.required],
     })
+
+    let id = this.activatedRoute.snapshot.paramMap.get('id')
+
+    if (id){
+      let reservation = this.reservationService.getReservation (id)
+
+      if(reservation)
+      this.reservationForm.patchValue (reservation)
+    }
     
   }
 
